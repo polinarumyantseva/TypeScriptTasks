923d8063f90c47c65b438e3bd4be84f002e810cf
polinarumyantseva <polinrumyantseva@gmail.com>
2025-09-03 14:57:51 -0700

    feat: Задание 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { totalPrice } from './task1.js';
+import { normalizeData, posts } from './task2.js';
 
 // Задание 1
 const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
 console.log(`Стоимость с учетом скидки и рассрочки: ${price}`); // 6250
+
+// Задание 2
+
+console.log(normalizeData(posts));
+/**
+ * {
+ *    byId: {
+ *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
+ *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
+ *      ...
+ *    },
+ *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
+ * }
+ */
