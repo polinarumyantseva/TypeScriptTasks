0a5891d1c7cfc5dac532f209f23c00fb0a169d60
polinarumyantseva <polinrumyantseva@gmail.com>
2025-09-11 10:36:39 -0700

    fix: добавляет проверки на правильность входных данных

--- src/task1.ts
+++ src/task1.ts
@@ -8,6 +8,17 @@ interface ProductProps {
 }
 
 export const totalPrice = ({ price, discount, isInstallment, months }: ProductProps): number => {
+	if (price < 0) {
+		throw new Error('Цена должна быть положительным числом');
+	}
+
+	if (discount < 0 || discount > 100) {
+		throw new Error('Скидка должна быть в диапазоне от 0 до 100');
+	}
+
+	if (months <= 0) {
+		throw new Error('Количество месяцев должно быть положительным числом');
+	}
 	const discountSize = (price * discount) / 100;
 	return isInstallment ? (price - discountSize) / months : price - discountSize;
 };
